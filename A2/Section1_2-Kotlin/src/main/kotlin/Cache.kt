5174f3b96aca920b269f60bebdaa15e52691889f
Miguel <SHUTUP@M.com>
2026-04-02 10:41:20 +0100

    1.2 extra done

--- A2/Section1_2-Kotlin/src/main/kotlin/Cache.kt
+++ A2/Section1_2-Kotlin/src/main/kotlin/Cache.kt
@@ -56,4 +56,23 @@ class Cache<K : Any, V: Any> {
     fun snapshot(): Map<K, V> {
         return storage.toMap()
     }
+
+    // recebo uma função predicate e devolvo um map<k,v>
+    // predicate: recebe um valor V e devolve true ou false
+    fun filterValues(predicate: (V) -> Boolean): Map<K, V> {
+        val result = mutableMapOf<K, V>() // criamos um novo map
+
+        // entry é declarado aqui e para cada elemento de storage chamamos de entry
+        // storage é um MutableMap, logo cada entry é um par (key, value)
+        for(entry in storage) { // percorremos todos os keys e valores
+            val key = entry.key // armazenamos
+            val value = entry.value // armazenamos
+
+            if (predicate(value)) { // chamamos a função e verificamos
+                result[key] = value // armazenamos no novo map
+            }
+        }
+
+        return result
+    }
 }
