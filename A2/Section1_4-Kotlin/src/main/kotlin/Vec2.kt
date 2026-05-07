ec12091d1f922911c7f72c10b78b7973c8d9915e
Miguel <SHUTUP@M.com>
2026-05-07 18:34:01 +0100

    Comments

--- A2/Section1_4-Kotlin/src/main/kotlin/Vec2.kt
+++ A2/Section1_4-Kotlin/src/main/kotlin/Vec2.kt
@@ -1,5 +1,7 @@
 package org.example
 
+// usamos a implementação de comparable para o kotlin saber como ordenar os vbetores e resolver operadores de comp
+// com comparable podemos associar <, >, <= e >= a função compareTo
 data class Vec2(val x: Double, val y: Double): Comparable<Vec2> {
 
     /*operator fun component1(): Double = x
@@ -13,6 +15,8 @@ data class Vec2(val x: Double, val y: Double): Comparable<Vec2> {
         return Vec2(x - other.x, y - other.y)
     }
 
+    // função membro da classe vec2
+    // o obj a esquerda (vec2) 
     operator fun times(scalar: Double): Vec2 {
         return Vec2(x * scalar, y * scalar)
     }
@@ -24,6 +28,7 @@ data class Vec2(val x: Double, val y: Double): Comparable<Vec2> {
     // compareTo retorna negativo se this < other
     // retorna 0 se são iguais
     // positivo se this > other
+    // magnitude = sqrt(x^2 + y^2)
     override operator fun compareTo(other: Vec2): Int {
         return this.magnitude().compareTo(other.magnitude())
     }
@@ -58,6 +63,9 @@ data class Vec2(val x: Double, val y: Double): Comparable<Vec2> {
     // temos 'V * a' e queremos 'a * V' por isso temos de "ensinar" Double a fazer isso
 }
 
+
+// aqui extendemos a classe Double para aceitar inputs do genero 2*vec2. ja que se usassemos
+// o outro metodo com 2*vec2 nao iria funcionar, pois a ordem importa
 operator fun Double.times(v: Vec2): Vec2 {
     return Vec2(this * v.x, this * v.y)
 }
