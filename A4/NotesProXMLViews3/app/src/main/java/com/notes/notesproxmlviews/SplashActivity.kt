ef71d1f3dd1cbc152d9c89292529c37d4287cd24
Miguel <SHUTUP@M.com>
2026-06-10 09:13:08 +0100

    a

---
+++ A4/NotesProXMLViews3/app/src/main/java/com/notes/notesproxmlviews/SplashActivity.kt
@@ -0,0 +1,25 @@
+package notes.pro
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val currentUser = FirebaseAuth.getInstance().currentUser
+            if (currentUser == null) {
+                startActivity(Intent(this@SplashActivity, LoginActivity::class.java))
+            } else {
+                startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            }
+            finish()
+        }, 1000)
+    }
+}
